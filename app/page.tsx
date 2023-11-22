4c1dd3790ca897d44a37e83cddc5e778312300de
bishal <bishalpd@gmail.com>
2023-11-22 19:32:21 +0600

    few updates on bg color, expense chart box, expense tab

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,18 @@
 import data from "../expense-data.json";
 
 export default function Home() {
-  console.log(data);
-  return <main>this is helvetica</main>;
+  return (
+    <main className="page_wrapper">
+      <h1>Expense Chart</h1>
+      <div className="chart_box">
+        <h2>Expenses</h2>
+        <div className="expense_tabs">
+          <button className="active">1m</button>
+          <button>6m</button>
+          <button>1y</button>
+          <button className="active">all time</button>
+        </div>
+      </div>
+    </main>
+  );
 }
