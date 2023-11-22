1f1827a63100ea32e15b89d9c710906ce9d0061e
bishal <bishalpd@gmail.com>
2023-11-22 21:58:13 +0600

    displaying tab value from expense data json

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,41 @@
 "use client";
 
+import { useState } from "react";
 import data from "../expense-data.json";
 
+type ExpenseType = {
+  name: string;
+  value: number;
+  color: string;
+};
 export default function Home() {
+  const [expense, setExpense] = useState({
+    type: "1M",
+    categories: data["1M"],
+  });
+
+  const selectExpenseCategory = (type: string, category: ExpenseType[]) => {
+    setExpense({ type: type, categories: category });
+  };
+
   return (
     <main className="page_wrapper">
       <h1>Expense Chart</h1>
       <div className="chart_box">
         <h2>Expenses</h2>
         <div className="expense_tabs">
-          <button className="active">1m</button>
-          <button>6m</button>
-          <button>1y</button>
-          <button className="active">all time</button>
+          {Object.keys(data).map((item: string, index: number) => {
+            return (
+              <button
+                type="button"
+                key={index}
+                onClick={() => selectExpenseCategory(item, (data as any)[item])}
+                className={expense.type === item ? "active" : ""}
+              >
+                {item}
+              </button>
+            );
+          })}
         </div>
       </div>
     </main>
